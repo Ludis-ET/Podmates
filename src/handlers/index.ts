e2a35cfa1de217901244b8c87b5d6b2a38729600
Ludis <leulsegedmelaku1020@gmail.com>
2024-12-21 19:51:32 +0300

    Initial commit: add bot structure, configuration, and remove unused files

--- src/handlers/index.ts
+++
@@ -1,7 +0,0 @@
-export { editPodcast } from './managePodcast';
-export { addPodcast } from "./addPodcast";
-export { handleGetStarted, handleContact, handleStartCommand } from "./welcome";
-
-export { handleBackHome } from "./main";
-
-export { sharePodcasts } from "./sharePodcast";
